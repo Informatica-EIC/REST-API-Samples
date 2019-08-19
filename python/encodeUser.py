8def621b14e9293ad864d0cbcfac0c9538f5d0af
darren_wrigley <dwrigley@informatica.com>
2019-08-19 11:49:32 -0400

    added print statemtents to set http auth

--- python/encodeUser.py
+++ python/encodeUser.py
@@ -1,42 +1,47 @@
-'''
+"""
 Created on Aug 10, 2018
 
-base64 encode - prompting for security domain, username and password
-this is useful for generating the string used for basic authentication using the rest api
+Note:  this is the python 3.x version
 
-the user will be prompted for an id and password & write the encoded result to the console
+base64 encode - prompting for security domain, username and password
+this is useful for generating the string used for API basic authentication
+
+the user will be prompted for an id and password and optional security domain
+& print the encoded result to the console
 
 @author: dwrigley
-'''
-
+"""
 
 import base64
 import getpass
-import platform
 
-
-print("python version=" + platform.python_version())
-if str(platform.python_version()).startswith("2.7"):
-    secDomain = raw_input("security domain <empty> for Native:")
-    u = raw_input("User Name for catalog:")
-    p = getpass.getpass(prompt='password for user=' + u + ':')
-    if secDomain == "":
-        auth_str = '%s:%s' % (u, p)
-    else:
-        auth_str = '%s\%s:%s' % (secDomain, u, p)
-    b64_auth_str = base64.b64encode(bytes(auth_str))
+# assume python 3.x (input vs raw_input)
+print("")
+print("Enter values for Security Domain (optional), User and Password")
+secDomain = input("security domain <empty> for Native:")
+u = input("User Name for catalog:")
+p = getpass.getpass(prompt="password for user=" + u + ":")
+if secDomain == "":
+    auth_str = f"{u}:{p}"
 else:
-    # assume 3.x
-    secDomain = input("security domain <empty> for Native:")
-    u = input("User Name for catalog:")
-    p = getpass.getpass(prompt='password for user=' + u + ':')
-    if secDomain == "":
-        auth_str = '%s:%s' % (u, p)
-    else:
-        auth_str = '%s\%s:%s' % (secDomain, u, p)
-    b64_auth_str = base64.b64encode(bytes(auth_str, 'utf-8'))
-    
-#print(auth_str)
-print(b64_auth_str.decode("utf-8"))
-print('header settings:')
-print('    "Authorization": "Basic ' + b64_auth_str.decode("utf-8") + '"')
+    auth_str = f"{secDomain}\\{u}:{p}"
+b64_auth_str = base64.b64encode(bytes(auth_str, "utf-8"))
+
+# print result of encoding to console
+# print(b64_auth_str.decode("utf-8"))
+print("header settings:")
+print(f'\t"Authorization": Basic {b64_auth_str.decode("utf-8")}')
+
+print("\nto set an env variable:")
+print(f'\texport INFA_EDC_AUTH="Basic {b64_auth_str.decode("utf-8")}"')
+print("")
+print("or add to launch.json for vsvode debugging - in the configurations section")
+print('\t"envFile": "${workspaceFolder}/.env",')
+print(f'\tand add\n\tINFA_EDC_AUTH="Basic {b64_auth_str.decode("utf-8")},"')
+print("\tto .env in the root folder of your project")
+print("")
+print("or add to launch.json for vscode debugging - in the configurations section")
+print(f'\t"env" : {{"INFA_EDC_AUTH" : "Basic {b64_auth_str.decode("utf-8")}"}},')
+
+print("")
+print("finished")
